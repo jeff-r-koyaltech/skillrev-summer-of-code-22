cb857f3e540e9d4413828356db74941959151a13
Jeff R <jeff@skillrev.dev>
2022-06-20 14:38:24 -0500

    adding login state

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    loggedInUser: '',
   },
   getters: {
+    isLoggedIn(state) {
+      return state.loggedInUser !== '';
+    },
   },
   mutations: {
   },
